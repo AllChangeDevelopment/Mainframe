c9b87f249a515f37b939a3fb1518d6b21fbe6abe
iwl-lyam <64089164+iwl-lyam@users.noreply.github.com>
2023-08-20 14:47:49 +0000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,190 @@
+import WebSocket from 'ws'
+import fetch from 'node-fetch'
+
+const nv = process.env['nv']
+const v = process.env['v']
+const vf = process.env['vf']
+const token = process.env['token']
+
+
+
+const link = "wss://gateway.discord.gg/?v=10&encoding=json"
+
+const ws = new WebSocket(link);
+
+const request = async (endpoint, method, body) => {
+  let req;
+  if (body !== null) {
+    req = await fetch("https://discord.com/api/v10"+endpoint, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bot "+token
+      }
+    })
+  } else {
+    req = await fetch("https://discord.com/api/v10"+endpoint, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bot "+token
+      }
+    })
+  }
+  try {let res = await req.json()
+       console.log(res)
+  return res} catch {}
+}
+
+export default function bot(loop,db) {
+  ws.on('error', error => {
+    console.log("Error")
+    console.error(error)
+  });
+  
+  ws.on('close', (code, reason) => {
+    console.log("Code "+code)
+    console.log(reason.toString())
+  })
+  
+  ws.on('open', function open() {
+  });
+  
+  let interval = 0
+  let ident = false
+  
+  ws.on('message', async function message(data) {
+    data = JSON.parse(data)
+    console.log(data)
+    switch (data.op) {
+      case 10:
+        interval = data.d.heartbeat_interval
+        ws.send(JSON.stringify({
+          "op": 1,
+          "d": null
+        }))
+        ident = false
+        break;
+      case 11:
+        if (!ident) {
+          console.log("Identify")
+          ws.send(JSON.stringify({
+            op: 2,
+            d: {
+              token: token,
+              properties: {
+                os: "windows",
+                browser: "whatever this shit is",
+                device: "whatever this shit is"
+              },
+              intents: 3276799
+            }
+          }))
+          ident = true
+        }
+        setTimeout(() => {
+          ws.send(JSON.stringify({
+            "op": 1,
+            "d": null
+          }))
+        },interval)
+        break;
+      case 1:
+        ws.send(JSON.stringify({
+          "op": 1,
+          "d": null
+        }))
+        break;
+      case 0:
+        if (data.t==='INTERACTION_CREATE') {
+          let interaction = data.d
+          if (interaction.data.name == "verify") {
+            let code = Math.floor(100000 + Math.random() * 900000)
+            db.insertOne({
+              interaction: interaction,
+              code: code
+            })
+            request(`/interactions/${interaction.id}/${interaction.token}/callback`, "POST", {type:4,data:{"embeds": [
+    {
+      "type": "rich",
+      "title": `Verification process`,
+      "description": `To verify click the link above and enter the code ${code}. This one-time code is valid for 15 minutes. Do not share this code with anyone except the game above. Please don't dismiss this message as the next steps will be shown here. If you dismiss it by accident, you can always restart the process by running /verify again.`,
+      "color": 0xff0000,
+      "fields": [
+        {
+          "name": `Status`,
+          "value": `Awaiting code input (${code})`
+        }
+      ],
+      "url": `https://roblox.com/games/14458182082/da-verification-thing`
+    }
+  ],flags:64}}, false)
+          }
+        }
+        if (data.t==="GUILD_MEMBER_ADD") {
+          let user = data.d.user
+console.log(await request(`/guilds/${data.d.guild_id}/members/${user.id}`,"PATCH",{roles:[nv]}))
+        }
+    }
+  });
+
+  loop.on('msg', async msg => {
+    console.log("Code: "+msg.code)
+    console.log("Polling: "+msg.polling)
+    
+    let person = await db.findOne({code:msg.code})
+    console.log(person)
+    if (!person) return
+    if (msg.polling.includes("Discord")) { 
+      request(`/webhooks/1138208598116282489/${person.interaction.token}/messages/@original`,"PATCH",{"embeds": [
+    {
+      "type": "rich",
+      "title": `Verification process`,
+      "description": `You have been verified. This message can be safely dismissed.`,
+      "color": 0xff0000,
+      "fields": [
+        {
+          "name": `Status`,
+          "value": `Verification passed`
+        }
+      ],
+      "url": `https://roblox.com/games/14458182082/da-verification-thing`
+    }
+  ],flags:64}, false)  
+let roles =       await request(`/guilds/${person.interaction.guild_id}/members/${person.interaction.member.user.id}`,"GET",null)
+      roles = roles.roles
+      roles[roles.indexOf(nv)] = v
+      console.log(await request(`/guilds/${person.interaction.guild_id}/members/${person.interaction.member.user.id}`,"PATCH",{roles:roles}))
+    }
+    else {request(`/webhooks/1138208598116282489/${person.interaction.token}/messages/@original`,"PATCH",{"embeds": [
+    {
+      "type": "rich",
+      "title": `Verification process`,
+      "description": `We have been unable to verify your account, so you will need to contact TFA support for further information. This message can be safely dismissed.`,
+      "color": 0xff0000,
+      "fields": [
+        {
+          "name": `Status`,
+          "value": `Contact TFA support`
+        }
+      ],
+      "url": `https://roblox.com/games/14458182082/da-verification-thing`
+    }
+  ],flags:64}, false)
+    let roles =       await request(`/guilds/${person.interaction.guild_id}/members/${person.interaction.member.user.id}`,"GET",null)
+      roles = roles.roles
+      roles[roles.indexOf(nv)] = vf
+      console.log(await request(`/guilds/${person.interaction.guild_id}/members/${person.interaction.member.user.id}`,"PATCH",{roles:roles}))
+         }
+
+    db.deleteOne({code:msg.code})
+    
+  })
+
+  
+  loop.on('cmd', async cmd => {
+    request("/applications/1138208598116282489/commands","POST",cmd)
+  })
+
+}
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb'
+const uri = "mongodb+srv://ws:websocket@cluster0.wo5l2jp.mongodb.net/?retryWrites=true&w=majority";
+import bot from './bot.js'
+import {EventEmitter} from 'events'
+const MessageLoop = new EventEmitter
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+client.connect()
+let Db = client.db("websocket")
+let db = Db.collection("messages");
+
+const app = express();
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  let s = await db.find({}).toArray()
+  res.send(s);
+});
+
+app.post('/commands', async (req,res) => {
+  MessageLoop.emit("cmd",req.body)
+  res.send(req.body)
+})
+
+app.post('/', async (req, res) => {
+  await db.insertOne(req.body)
+  MessageLoop.emit("msg",req.body)
+  res.send(req.body)
+})
+
+app.listen(3000, () => {
+  console.log('Express server initialized');
+  bot(MessageLoop,Db.collection("waiting"))
+});
