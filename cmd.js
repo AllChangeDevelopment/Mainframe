f122705ed4607a067a0d8d3b93af2cfe1f2d11ae
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-02-17 17:52:15 +0000

    permissions

--- cmd.js
+++ cmd.js
@@ -15,14 +15,16 @@ export default async function cmd(body) {
     await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
         name: unban.title,
         description: unban.description,
-        options: unban.args
+        options: unban.args,
+        default_member_permissions: "4"
     })
 
     cmdLogger.info("Registering /ban")
     await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
         name: ban.title,
         description: ban.description,
-        options: ban.args
+        options: ban.args,
+        default_member_permissions: "4"
     })
 
     setTimeout(async () => {
@@ -31,32 +33,34 @@ export default async function cmd(body) {
         await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
             name: kick.title,
             description: kick.description,
-            options: kick.args
+            options: kick.args,
+            default_member_permissions: "2"
         })
 
+        cmdLogger.info("Registering /mute")
+        await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
+            name: mute.title,
+            description: mute.description,
+            options: mute.args,
+            default_member_permissions: "1099511627776"
+        })
 
         setTimeout(async () => {
-
-
-            cmdLogger.info("Registering /mute")
-            await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
-                name: mute.title,
-                description: mute.description,
-                options: mute.args
-            })
 
             cmdLogger.info("Registering /unmute")
             await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
                 name: unmute.title,
                 description: unmute.description,
-                options: unmute.args
+                options: unmute.args,
+                default_member_permissions: "1099511627776"
             })
 
             cmdLogger.info("Registering /warnings")
             await request(`/applications/${process.env.CID}/commands`, "POST", {}, {
                 name: warnings.title,
                 description: warnings.description,
-                options: warnings.args
+                options: warnings.args,
+                default_member_permissions: "1099511627776"
             })
         }, 5000)
     }, 5000)
