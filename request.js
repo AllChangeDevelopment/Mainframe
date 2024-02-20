7c51596bb360c909cededa56db839b5c65e82583
iwl-lyam <lyam.mosnier@itwithlyam.co.uk>
2024-02-20 21:12:21 +0000

    Fix JS-D1001 - add documentation to request()

--- request.js
+++ request.js
@@ -3,6 +3,14 @@ import dotenv from 'dotenv'
 dotenv.config({path: "./secrets.env"})
 import {reqLogger} from './logger.js'
 
+/**
+ * Sends a request to the Discord API
+ * @param {string} endpoint
+ * @param {string} [method="GET"] method
+ * @param {Object} [headers={}] headers
+ * @param {Object} [body=null] body
+ * @returns Object
+ */
 export default async function request(endpoint, method="GET", headers={}, body=null) {
     reqLogger.http(`Sending ${method} request to ${endpoint}`)
     const req = await fetch(`https://discord.com/api/v10${endpoint}`, {
@@ -29,5 +37,5 @@ export default async function request(endpoint, method="GET", headers={}, body=n
         reqLogger.warn("No JSON returned")
         return {}
     }
-    
+
 }
